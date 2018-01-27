d7903856638e0c6f16b625136e8b23bb934c0bf5
Amineh <ak9123a@american.edu>
2018-01-27 12:01:02 -0800

    updates on jan27

--- flaskexample/views.py
+++ flaskexample/views.py
@@ -19,36 +19,15 @@ import argparse
 import sys
 import time
 
+import json
+import urllib.parse
+import urllib.request
+import us
+
 import numpy as np
 import tensorflow as tf
 
 
-# TODO - update to fb calls or read from db
-style_to_info = {
-  'persian miniature' :
-     {'museum_name' : 'Philadelphia Museum of Art',
-      'website' : 'http://www.philamuseum.org/collections/permanent/63228.html',
-      'fb_events' : []},
-  'Japanese Kano School' :
-     {'museum_name' : 'Pulitzer Arts Foundation: Sumi-e Painting Workshop Presented by Japan House',
-      'website' : 'https://pulitzerarts.org/program/sumi-e-painting-workshop-presented-by-japan-house',
-      'fb_events' : ['https://www.facebook.com/events/877780825716060/']},
-  'abstract expressionism' :
-     {'museum_name' : 'Guggenheim Museum',
-      'website' : 'https://www.guggenheim.org/artwork/movement/abstract-expressionism',
-      'fb_events' : ['https://www.facebook.com/events/1775280619440369/']},
-  'Neo-expressionism' :
-     {'museum_name' : 'Guggenheim Museum',
-      'website' : 'https://www.guggenheim.org/artwork/movement/neo-expressionism',
-      'fb_events' : []},
-  'fauvism' :
-     {'museum_name' : 'MoMA',
-      'website' : 'https://www.moma.org/learn/moma_learning/themes/fauvism',
-      'fb_events' : []},
-  'cubism paintings' :
-     {'museum_name' : 'The Met',
-      'website' : 'https://www.metmuseum.org/exhibitions/listings/2014/cubism-leonard-a-lauder-collection',
-      'fb_events' : ['https://www.facebook.com/events/110512576391247/']}}
 # user = 'amineh' #add your username here (same as previous postgreSQL)
 # host = 'localhost'
 # dbname = 'birth_db'
@@ -106,7 +85,7 @@ def load_labels(label_file):
     label.append(l.rstrip())
   return label
 
-def call_model(file_name):
+def find_style(file_name):
   # TODO: move these to parameters
   file_path = UPLOAD_PREFIX + app.config['UPLOAD_FOLDER'] + file_name
   model_file = UPLOAD_PREFIX + "static/models/retrained_graph.pb"
@@ -138,82 +117,115 @@ def call_model(file_name):
     end=time.time()
   results = np.squeeze(results)
 
-  top_k = results.argsort()[-5:][::-1]
+  #top_k = results.argsort()[-5:][::-1]
+  top = results.argsort()[-1]
   labels = load_labels(label_file)
-
   print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
 
-  result = []
-  for i in top_k:
-    result.append({'label': labels[i],
-                   'score': results[i],
-                   'more_info' : (style_to_info[labels[i]] if labels[i] in style_to_info  else None)})
-  return result
+  return labels[top] if top < len(labels) else None
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'paintingfile' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['paintingfile']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            # TO DO: refactor properly before AWS for sure
-            print("path is:", UPLOAD_PREFIX + app.config['UPLOAD_FOLDER'] + filename)
-            file.save(UPLOAD_PREFIX + app.config['UPLOAD_FOLDER'] + filename)
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return render_template('upload-page.html')
+  if request.method == 'POST':
+    # check if the post request has the file part
+    if 'paintingfile' not in request.files:
+      flash('No file part')
+      return redirect(request.url)
+    file = request.files['paintingfile']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+      flash('No selected file')
+      return redirect(request.url)
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      # TO DO: refactor properly before AWS for sure
+      print("path is:", UPLOAD_PREFIX + app.config['UPLOAD_FOLDER'] + filename)
+      file.save(UPLOAD_PREFIX + app.config['UPLOAD_FOLDER'] + filename)
+      return redirect(url_for('uploaded_file',
+                              filename=filename))
+  return render_template('upload-page.html')
 
 @app.route('/about')
 def about_us():
   return render_template('about-us.html')
 
 def get_fb_token():
-  return 'EAAImEswoEjEBABqwxQZCSggIZCo62S86xKkpLDkaMMvCZBQ1Vex7qR4wwo4BpZB0CcjZBVO4OjM4yRyvfk5Ey8N2ItipSmMNVOhVYAetGA4EFTPVASZAcXW8RZBtncuaFqZA0BxA6ZCaOycNS7lXu9g2oT8PEdaNOvkInyT8ROUZCd6nN7kgytNsESvIXOtOqfJR2f5V3ENy6k7gZDZD'
+  # app_id = '410719039365960',
+  # app_secret = 'd230b52f340fb18d80c6dcdf4dbfb27e'
+  # payload = {'grant_type': 'client_credentials', 'client_id': app_id, 'client_secret': app_secret}
+  # file = requests.post('https://graph.facebook.com/oauth/access_token?', params = payload)
+  # result = json.loads(file.text)['access_token']
+  result = 'EAAB7ZAIUemhABADDwiIpQfMb2RLeJP8cwH9yBv05XGUgPma24uoZCZB8iF8m2kaQVEIsFw5zL4P4W8kPosKCgU6C5XZAlZCTvk1Um0irdyhZCJZBIQNqfaZBTn24FZAThqtQI8H6aqXTpfZBosdPBeb2VG0BiHS05oJ3EaXsGE5K1TIgZDZD'
+  return result
 
-# mikhaim begim age kasi zad address e amazon ya hamin localhost/eventlist biad inja
-# inke che joori miad be in, mohem nist, yani nadooni ham hich tasiri nadare, bedooni ham hamintor
-# handles http://localhost/eventlist
-# bring up your server
 @app.route('/eventlist')
-def retrieve_event_list():
+def find_fb_events(style):
   # reading request parameters (i.e. the ones that come after ?blah=foo&blah2=foo1)
   #http://flask.pocoo.org/docs/0.12/quickstart/
-  query = request.args.get('query', '')
-  graph = facebook.GraphAPI(access_token=get_fb_token(), version = 2.7)
-  events = graph.request('/search?q=' + query + '&type=event&limit=100')
-  eventList = events['data']
-  results = {}
-  for i in eventList:
-    country=i['place']['location']['country'] if 'place' in i and 'location' in i['place'] and 'country' in i['place']['location'] else None
-    if country=='United States':
+  events = []
+  try:
+    graph = facebook.GraphAPI(access_token=get_fb_token(), version = 2.7)
+    events = graph.request('/search?q=' + style + '&type=event&limit=100')
+    eventList = events['data']
+    events = {}
+    print(len(eventList))
+    for i in eventList:
+      country=i['place']['location']['country'] if 'place' in i and 'location' in i['place'] and 'country' in i['place']['location'] else None
+      # if country=='United States':
+      event_id=(i['id'])
+      location = i['place']['location']['city'] + ', ' + i['place']['location']['state'] if country=='United States' else country
+      events[event_id] = {
+          'title' : i['name'],
+          'location' : location,
+          'link' : 'https://www.facebook.com/events/'+event_id+'/'
+      }
+  except Exception as e:
+    print(e)
+  return events
 
-        # result={}
-        event_id=(i['id'])
-        # result[event_id]=[i['name'],i['place']['location']['city'],'https://www.facebook.com/events/'+event_id+'/']
-        results[event_id] = {
-            'name' : i['name'],
-            'city' : i['place']['location']['city'],
-            'link' : 'https://www.facebook.com/events/'+event_id+'/'
-        }
-  # return the list to an html page which is under templates folder
-  # this says render/process eventlist.html file, and I give you a variable name events
-  # which has 'results' now let's create eventlist.html
-  return render_template('eventlist.html', events=results)
+
+def find_museums(style, result_count=20):
+  api_key = 'AIzaSyDQ57s9pTYh8yi3jwMXY_A2ZIzYc27ds9s'
+  service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+  params = {
+      'query': style,
+      'limit': result_count,
+      'indent': True,
+      'key': api_key,
+      'types':'Organization',
+      # 'types':Event
+  }
+  url = service_url + '?' + urllib.parse.urlencode(params)
+  response = json.loads(urllib.request.urlopen(url).read())
+  state_names = [state.name for state in us.states.STATES_AND_TERRITORIES]
+  results=[]
+  for element in response['itemListElement']:
+      description=element['result']['description'] if 'description' in element['result'] else None
+      url=element['result']['url'] if 'url' in element['result'] else None
+      name=element['result']['name'] if 'name' in element['result'] else None
+      city=element['result']['city'] if 'city' in element['result'] else None
+      sec_url=element['result']['detailedDescription']['url'] if 'detailedDescription' in element['result'] and 'url' in element['result']['detailedDescription'] else None
+      if url is None:
+          url=sec_url
+      if description is not None:
+          words=description.split()
+          state=words[-1]
+          if state in state_names:
+              results.append({'link': url , 'title': name, 'city' : city })
+  return results
+
+@app.route('/museumlist')
+def museums_page():
+  museums = get_museum_list(request.args.get('query', ''))
+  return get_render_template('museumlist.html', events=results)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    result = call_model(filename)
-    return render_template('results.html', classification_results=result, filename=filename, wikipedia_link='', website_link='')
-
+  style = find_style(filename)
+  museums = find_museums(style)
+  events = find_fb_events(style)
+  return render_template('results.html',fb_events=events, museums= museums, style=style, filename=filename)
 
 
 
